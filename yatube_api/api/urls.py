9283428c60e58e3aaf8d14e7592568d178b1c393
Даниил Ряйккенен <dramshtadiy1337@gmail.com>
2023-04-05 23:33:05 +0300

    8sp

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,16 +1,18 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
-router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
+router = DefaultRouter()
+
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'groups', GroupViewSet, basename='Group')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comment')
 
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path("v1/api-token-auth/", views.obtain_auth_token),
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post
-from rest_framework import permissions, viewsets
+from posts.models import Group, Post
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-
+from api.permissions import IsOwnerOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -28,7 +21,6 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
 
